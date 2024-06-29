050a1ac02b26e9de7517a372b56ced45687ff92e
Ian Oliveira <ianwandersong12@gmail.com>
2024-06-29 15:16:42 -0300

    added wireframe

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # movie_recommender_app
 
-A new Flutter project.
-
+prototipo
+<img src="gemini_app.png" />
 ## Getting Started
 
 This project is a starting point for a Flutter application.
--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // try changing the seedColor in the colorScheme below to Colors.green
         // and then invoke "hot reload" (save your changes or press the "hot
         // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
+        // the command line to start the app).c
         //
         // Notice that the counter didn't reset back to zero; the application
         // state is not lost during the reload. To reset the state, use hot
