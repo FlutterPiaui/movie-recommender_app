7a756e5e5d71f51da7916b74a5aa74cd0e3e4438
Ian Oliveira <ianwandersong12@gmail.com>
2024-07-20 20:19:44 -0300

    added firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,49 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:movie_recommender_app/firebase_options.dart';
 import 'package:movie_recommender_app/src/core/theme/app_theme.dart';
 import 'package:movie_recommender_app/src/di/di_setup.dart';
 import 'package:movie_recommender_app/src/routes/routes.dart';
 
 void main() async {
-  await configureDependencies();
-  final deviceLanguage = Platform.localeName.split('_')[0];
-  await AppLocalizations.delegate.load(Locale(deviceLanguage));
-  runApp(const MyApp());
+  runZonedGuarded<Future<void>>(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+      await FirebaseCrashlytics.instance
+          .setCrashlyticsCollectionEnabled(!kDebugMode);
+
+      FlutterError.onError = (details) {
+        FlutterError.presentError(details);
+
+        final information = details.informationCollector?.call() ?? [];
+
+        FirebaseCrashlytics.instance.recordError(
+          details.exceptionAsString(),
+          details.stack,
+          reason: details.context?.toStringDeep(minLevel: DiagnosticLevel.info),
+          information: information,
+          printDetails: false,
+          fatal: true,
+        );
+      };
+      await configureDependencies();
+      final deviceLanguage = Platform.localeName.split('_')[0];
+      await AppLocalizations.delegate.load(Locale(deviceLanguage));
+      runApp(const MyApp());
+    },
+    (error, stack) =>
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
+  );
 }
 
 class MyApp extends StatelessWidget {
